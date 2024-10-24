d43ebf0c8c275b3cf3cd95817732b54dfa03a541
AnjanaK93 <287968@ust.com>
2024-10-24 15:20:31 +0530

    first commit

---
+++ src/main/java/com/ust/APIGateway/filter/RouteValidator.java
@@ -0,0 +1,20 @@
+package com.ust.APIGateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth/validate/user",
+            "/api/auth/register",
+            "api/auth/validate/token"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured = request->openApiEndpoints.stream().noneMatch(uri->request
+            .getURI().getPath().contains(uri));
+}
